6baf1e387d7194121cab331b2ca41053cc122a9b
Louis Silverstein <louis@gripreality.com>
2022-09-09 20:35:32 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# how to write things in python use camelCase = what is this? It is writing out words or phrases
+# using lower and upper case letters all in one unit without spaces eg. "thisIsAnExample"
+
+# in python and computers in general spaces are not your friend. But underscore "_"
+# is your friends.
+
+# Basic variable
+
+# Type of variables are numbers, strings, arrays, dictionary, tuples
+
+iAmANumber = 546          # a number is a number, pretty obvious. You can not make a number a variable.
+#4 = 0                  # this does not work
+
+iAmAString = "a word or something like that"    # a string is text
+
+iAmAnArray = ["cat", "dog", "pig", 5]          # an array is a group of variables
+#               0      1      2    3
+
+iAmADictionary = {"key":"value", "animals" : ["dog", "cat", "mouse"], "People":5}    # a dictionary is a
+                                                # group of objects, and what is an object?
+                                                # an object is any type of variable.
+
+iAmATuple = (3,6,10)       # a tuple is an immutable array (it is an array that you can't change)
+
+iAmAChar = "f"              # using a character like a number eg. 'f' in hex = 14.
+
+print(iAmADictionary["animals"][2])
+
+"""
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
